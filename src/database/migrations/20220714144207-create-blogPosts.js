48a6fcff469edb0db2633a4f83e086ae2371d2c6
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-14 12:22:40 -0300

    feat: Requisito 1 - Cria as migrations para as entidades User, Categories, BlogPosts, PostCategories
    Cria a migration de cada entidade fazendo as referencias necessarias na BlogPosts que tem uma foreign key de userId e na PostCategories que é uma tabela de junção entre as tabelas de BlogPosts e Categories

---
+++ src/database/migrations/20220714144207-create-blogPosts.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+   /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('BlogPosts', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    content: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    published: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updated: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+   })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
