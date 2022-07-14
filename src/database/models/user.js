57afcac7cd15ebc7e042463cf922ae72a124aee4
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-14 14:28:26 -0300

    feat: Cria o modelo User com suas propriedades
    Cria a model User no diretorio src/database/models/user.js respeitando o diagrama de Entidade-Relacionamento e o formato das entidades

---
+++ src/database/models/user.js
@@ -0,0 +1,32 @@
+/** @type {import('sequelize').ModelAttributes} */
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  });
+
+  return User;
+};
+
+module.exports = User;
