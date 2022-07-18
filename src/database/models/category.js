c3b4c94d11d759f6cb4eff8fe3956b47a911ea1d
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-18 18:26:52 -0300

    feat: Requisito 7 - Cria o model Category com as propriedades corretas
    O model deve possuir as propriedades id e name e estar no diretorio 'src/database/models/category.js'

---
+++ src/database/models/category.js
@@ -0,0 +1,22 @@
+/** @type {import('sequelize').ModelAttributes} */
+
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  }, {
+    timestamps: false
+  });
+
+  return Category;
+};
+
+module.exports = Category;
